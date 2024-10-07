e9f114553de77447858f50cf83f2a14f948b21b5
Matt DiDomenico <babsonmatt@gmail.com>
2024-10-07 13:19:50 -0400

    remove whitespace

--- components/demo.tsx
+++ components/demo.tsx
@@ -10,7 +10,6 @@ export function Demo() {
       <Button variant="outline" onClick={() => setShowModal(true)}>
         Delete Deployment
       </Button>
-
       <DeleteDialog
         showModal={showModal}
         setShowModal={setShowModal}
